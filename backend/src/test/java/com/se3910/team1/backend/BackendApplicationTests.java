0bd019eef603b24b6b944360259e8b925f9a71f9
Noah Dinwiddie <92384217+Noahdin04@users.noreply.github.com>
2026-03-28 23:47:46 -0500

    added springboot backend

---
+++ backend/src/test/java/com/se3910/team1/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se3910.team1.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
